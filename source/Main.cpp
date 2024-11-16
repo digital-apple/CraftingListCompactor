bd62fdcf5f4a0e528807098b3233ad24028540f2
digital-apple <115373190+digital-apple@users.noreply.github.com>
2024-11-16 13:02:52 -0300

    Prologue

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,3 +1,18 @@
+/*
+ * Goal: Compact the ConstructibleObjectMenu's underlying list by grouping entries with the same exact result:
+ * 
+ * A new step has to be implemented to the crafting process: After grouping entries with the same result into individual placeholder entries, 
+ * we must render a new list after selecting them, containing every individual entry that was compacted, but this comes with a few issues:
+ * 
+ * - We must parse the final ConstructibleObject list after every relevant plugin, such as "Crafting Recipe Distributor" has made it's changes to the data handler array.
+ * - The placeholder entry must not have any crafting information, but a description or detail that tells the final user that it is meant to be a "group" of entries.
+ * - Once a placeholder entry gets selected, we must render a new list containing only the relevant (grouped) entries, respecting the game's rules to disable (greyout) specific entries.
+ * - We must account for plugins that skip the confirmation box such as "Yes Im Sure".
+ * - Pressing the return button inside the grouped list should render the original list, instead of quitting from the menu.
+ */
+
+
+
 void InitializeLogger()
 {
     auto path = SKSE::log::log_directory();
